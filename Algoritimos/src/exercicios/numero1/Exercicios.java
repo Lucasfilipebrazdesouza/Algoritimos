fc3ab24128f7650da374dc9e642fae066ed0faf5
Noturno <Noturno@192.168.7.22>
2017-05-18 17:53:20 -0300

    commit inicial

---
+++ Algoritimos/src/exercicios/numero1/Exercicios.java
@@ -0,0 +1,12 @@
+package exercicios.numero1;
+
+public class Exercicios {
+public static void main(String[] args) {
+	int numeroA = 300;
+	int numeroB = 200;
+	int soma = numeroA + numeroB;
+	System.out.println(+soma);
+}	
+}
+
+
---
+++ Algoritimos/src/exercicios/numero2/Exercicios02.java
@@ -0,0 +1,13 @@
+package exercicios.numero2;
+
+public class Exercicios02 {
+public static void main(String[] args) {
+	 double var1 = 10000.00d;
+	 float  var2 = 10000.001f;
+	 double multiplicacao = var1 * var2;
+	 System.out.println(multiplicacao);
+	     	
+	 		}	
+	 		}
+
+	 
